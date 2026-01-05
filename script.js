b493df2cfc13d2d08069269369d7ee96ea28fdf7
viaarnas <viaardnas@gmail.com>
2026-01-05 18:35:24 +0700

    Update script.js

--- script.js
+++ script.js
@@ -23,6 +23,10 @@ noBtn.addEventListener("click", function() {
   modal.style.display = "none";
 });
 
+modal.addEventListener("click", function() {
+  modal.style.display = "none";
+});
+
 yesBtn.addEventListener("click", function() {
   modal.style.display = "none";
   isConfirmed = true;
